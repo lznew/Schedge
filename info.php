80793b0aedf84a9bd8704992239f0ff7801aeb07
lznew <support@lonniezanenewkirk.com>
2013-08-11 11:33:01 -0500

    info file upload

---
+++ info.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Schedge
+Plugin URI: https://github.com/lznew/schedge
+Description: Scheduling app
+Author: lznew
+Author URI: http://lonniezanenewkirk.com
+Version: 0.1-alpha
+*/
